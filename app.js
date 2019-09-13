c42c6b15a3b33157abd1d2f87b23526cf32b2918
cristina1084 <cristina.johns1084@gmail.com>
2019-09-13 17:52:12 +0530

    upload images

--- app.js
+++ app.js
@@ -9,6 +9,22 @@ var url = "mongodb://localhost/gallerydb"
 var signuprouter = require('./routes/signup');
 var loginrouter = require('./routes/login');
 
+app.use((req, res, next)=>{
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+mongo.connect(url,{useNewUrlParser:true}, (err)=>{
+  if(err) throw err;
+  else console.log("Database connected");
+})
+
+app.use(body.urlencoded({ extended: false }));
+app.use(body.json());
+
 var storage =   multer.diskStorage({  
     destination: (req, file, callback)=>{  
       callback(null, './public/images');  
@@ -18,29 +34,19 @@ var storage =   multer.diskStorage({
     }  
   });  
   
-var upload = multer({ storage : storage}).single('pimage');
-
-mongo.connect(url,{useNewUrlParser:true}, (err)=>{
-  if(err) throw err;
-  else console.log("Database connected");
-})
-
-
-app.use(body.urlencoded({ extended: false }));
-app.use(body.json());
-
-app.use((req, res, next)=>{
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
-});
+var upload = multer({ storage : storage});
 
 app.use("/signup", signuprouter);
 
 app.use("/login", loginrouter);
+ 
+app.post('/api', upload.any(), (req, res)=>{
+    console.log(req.files);
+    res.end('File is uploaded');
+})
+
 
 app.listen(process.env.PORT || 8080,()=>{
   console.log("Listening");
 })
+
