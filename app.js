419e82a997588225125dfac76149b3858277ced8
cristina1084 <cristina.johns1084@gmail.com>
2019-09-23 15:55:04 +0530

    minor change

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var albumrouter = require('./routes/album');
 var picturerouter = require('./routes/picture');
 
 app.use((req, res, next)=>{
-  res.setHeader('Access-Control-Allow-Origin', 'https://snapshots-site.herokuapp.com/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://snapshots-site.herokuapp.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', true);
